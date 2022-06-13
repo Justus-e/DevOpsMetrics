aa63dbbd8c02b1ee6fa0d0970c0cb12adfd378c5
Justus-e <56258792+Justus-e@users.noreply.github.com>
2022-06-13 17:52:11 +0200

    Test webhooks (#8)
    * test commit
    
    * test commit 2
    
    Co-authored-by: justus-e <justernst.je@gmail.com>

--- src/endpoints/githubHook.js
+++ src/endpoints/githubHook.js
@@ -47,7 +47,7 @@ const evaluateDeploymentEvent = (payload) => {
 };
 
 const evaluateIssueEvent = (payload) => {
-  //TODO: implement
+  //TODO: implement test test bla bla
 };
 
 module.exports = router;
