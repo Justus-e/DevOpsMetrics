90e8ab12e1c31e26d0f0b1927d8a800ee853683e
justus-e <justernst.je@gmail.com>
2022-06-30 12:57:29 +0200

    fix events endpoint + doc

--- README.md
+++ README.md
@@ -11,12 +11,34 @@ Api Docs: `http://<your_host>/swagger`
 
 ## Environment Variables
 
-| var               | description                                                                                                   | example                   |
-|:------------------|:--------------------------------------------------------------------------------------------------------------|:--------------------------|
-| **PORT**          | _optional_ - Port on which the API should be listening (default: 8080)                                        | 8080                      |
-| **API_KEY**       | _required_ - Authorization Key for this Api required to access all Endpoints                                  | 'SoMeRand0MS3curE5trinG'  |
-| **INFLUX_URL**    | _required_ - URL of the InfluxDB instance that should be used                                                 | 'http://localhost:8086'   |
-| **INFLUX_TOKEN**  | _required_ - Token to access InfluxDB (can be generated in the InfluxDB UI)                                   | 'AmfWLq5PbC_89NkpO\[...]' |
-| **INFLUX_ORG**    | _required_ - Organization name of the InfluxDB instance                                                       | 'DevOpsMetrics'           |
-| **INFLUX_BUCKET** | _required_ - Name of the InfluxDB bucket where the events should be stored                                    | 'events'                  |
-| **GITHUB_URL**    | _optional_ - GitHub URL of GitHub Server where the tracked Repo is stored (default: 'https://api.github.com') | 'https://api.github.com'  |
+| var               | description                                                                                                   | example                      |
+|:------------------|:--------------------------------------------------------------------------------------------------------------|:-----------------------------|
+| **PORT**          | _optional_ - Port on which the API should be listening (default: 8080)                                        | 8080                         |
+| **API_KEY**       | _required_ - Authorization Key for this Api required to access all Endpoints                                  | 'SoMeRand0MS3curE5trinG'     |
+| **INFLUX_URL**    | _required_ - URL of the InfluxDB instance that should be used                                                 | 'http://localhost:8086'      |
+| **INFLUX_TOKEN**  | _required_ - Token to access InfluxDB (can be generated in the InfluxDB UI)                                   | 'AmfWLq5PbC_89NkpO\[...]'    |
+| **INFLUX_ORG**    | _required_ - Organization name of the InfluxDB instance                                                       | 'DevOpsMetrics'              |
+| **INFLUX_BUCKET** | _required_ - Name of the InfluxDB bucket where the events should be stored                                    | 'events'                     |
+| **GITHUB_URL**    | _optional_ - GitHub URL of GitHub Server where the tracked Repo is stored (default: 'https://api.github.com') | 'https://api.github.com'     |
+| **GITHUB_USER**   | _optional_ - Needed if Repository is private or on an Enterprise Server                                       | 'justus-e'                   |
+| **GITHUB_OAUTH**  | _optional_ - Needed if Repository is private or on an Enterprise Server (can be generated in the GitHub UI)   | 'ghp_bitz8n81coxxnYvZ\[...]' |
+
+## Setup on GitHub
+### Create Personal access token
+
+_optional if the Repository is public and on the regular GitHub server_
+
+1. Go to your account `settings > Developer settings > Personal acess tokens > Generate new token`
+2. Expiration should be `no expiration` or a new token has to be created regularly
+3. `repo` scope should be selected
+~~4. _optional_ - `admin:repo_hook` can be selected to automate next step "Create Webhook"~~
+
+### Create Webhook
+
+1. Go to the page of the repository that should be tracked
+2. Go to `Settings > Webhooks > Add webhook`
+3. For Payload URL paste: `https://<your_host>/api/github-hook/`
+4. For content type select `application/json`
+5. For Secret paste your `API_KEY` that you set in your ENV-Vars
+6. Select `Let me select individual events`
+7. Check `Deployment statuses`, `Issues` and `Pushes`
--- src/endpoints/events.js
+++ src/endpoints/events.js
@@ -13,9 +13,99 @@ const influx = require("../influx");
  *                      type: string
  *                      required: true
  *                      description: type of event ('deployment' | 'change' | 'incident' | 'restore')
+ *                      example: "deployment"
  *                  payload:
- *                      type: object
+ *                      oneOf:
+ *                          - $ref: '#/components/schemas/deployment'
+ *                          - $ref: '#/components/schemas/change'
+ *                          - $ref: '#/components/schemas/incident'
+ *                          - $ref: '#/components/schemas/restore'
  *                      description: payload specific to the Event
+ */
+
+/** Event MODEL
+ * @swagger
+ * components:
+ *   schemas:
+ *     deployment:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           required: true
+ *           description: sha of the triggering commit for the deployment
+ *           example: "dd05f3119a18f53d1e782384b8f47751479de472"
+ *         changes:
+ *           type: array
+ *           items:
+ *             type: string
+ *           example: ["dd05f3119a18f53d1e782384b8f47751479de472", "79e10eec267bf99ec5a67acb28bc24b0eca1977a", "13fcc20de75badd00c2e1c94423a97383a4f2137"]
+ *           required: true
+ *           description: SHAs of all commits after last deployment commit until now
+ *         repo:
+ *           type: string
+ *           required: true
+ *           description: name of the repository
+ *           example: "justus-e/DevOpsMetrics"
+ *     change:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           required: true
+ *           description: sha of the commit
+ *           example: "dd05f3119a18f53d1e782384b8f47751479de472"
+ *         timestamp:
+ *           type: string
+ *           required: true
+ *           description: timestamp which should be used in calculation for this change
+ *           example: "2021-01-25T13:55:52Z"
+ *         ref:
+ *           type: string
+ *           required: true
+ *           description: branch name of commit
+ *           example: "refs/heads/main"
+ *         repo:
+ *           type: string
+ *           required: true
+ *           description: name of the repository
+ *           example: "justus-e/DevOpsMetrics"
+ *     incident:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           required: true
+ *           description: custom id, it is used to reference the same issue from incident and restore event
+ *           example: "1277125853"
+ *         timestamp:
+ *           type: string
+ *           required: true
+ *           description: timestamp which should be used in calculation for this incident
+ *           example: "2021-01-25T15:55:52Z"
+ *         repo:
+ *           type: string
+ *           required: true
+ *           description: name of the repository
+ *           example: "justus-e/DevOpsMetrics"
+ *     restore:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: string
+ *           required: true
+ *           description: custom id, it is used to reference the same issue from incident and restore event
+ *           example: "1277125853"
+ *         timestamp:
+ *           type: string
+ *           required: true
+ *           description: timestamp which should be used in calculation for this incident
+ *           example: "2021-01-26T12:31:12Z"
+ *         repo:
+ *           type: string
+ *           required: true
+ *           description: name of the repository
+ *           example: "justus-e/DevOpsMetrics"
  */
 
 /**
@@ -49,13 +139,13 @@ router.post("/events", (req, res) => {
         influx.writeDeploymentEvent(payload);
         break;
       case "change":
-        influx.writeChangeEvent();
+        influx.writeChangeEvent(payload);
         break;
       case "incident":
-        influx.writeIncidentEvent();
+        influx.writeIncidentEvent(payload);
         break;
       case "restore":
-        influx.writeRestoreEvent();
+        influx.writeRestoreEvent(payload);
         break;
       default:
         res.status(400).send("unknown eventType");
